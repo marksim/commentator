d0e19b1cfa7b378e6954b474acea5f0892a3f665
Mark Sim <marksim@gmail.com>
2013-08-30 13:22:22 -0500

    Fix problem with reloading CommentsHelper

--- lib/commentator/engine.rb
+++ lib/commentator/engine.rb
@@ -1,5 +1,14 @@
 module Commentator
   class Engine < ::Rails::Engine
     isolate_namespace Commentator
+    initializer 'commentator' do |app|
+      ActiveSupport.on_load(:action_view) do
+        require "commentator/comments_helper"
+        class ActionView::Base
+          include Commentator::CommentsHelper
+        end
+      end
+    end
+
   end
 end
