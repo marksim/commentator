269aa2b81299d1289e4d1c186f10faddce451b37
Mark Sim <marksim@gmail.com>
2013-09-18 16:29:08 -0500

    Allows multiple comments_for on the same screen without inserting the comment into every top level when posted.

--- app/helpers/commentator/application_helper.rb
+++ app/helpers/commentator/application_helper.rb
@@ -1,4 +1,25 @@
 module Commentator
   module ApplicationHelper
+    def slugify(string)
+      ret = string.strip.downcase
+
+      #blow away apostrophes
+      ret.gsub!(/['`]/, "")
+
+      # @ --> at, and & --> and
+      ret.gsub!(/\s*@\s*/, " at ")
+      ret.gsub!(/\s*&\s*/, " and ")
+
+      #replace all non alphanumeric, dash with dash
+      ret.gsub!(/\s*[^A-Za-z0-9\-]\s*/, '-')
+
+      #convert double underscores to single
+      ret.gsub!(/\-+/, "-")
+
+      #strip off leading/trailing underscore
+      ret.gsub!(/\A[-\.]+|[-\.]+\z/, "")
+
+      ret
+    end
   end
 end
